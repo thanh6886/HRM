d56dfb00c008b8cad805dc3f59a2323aeb567a26
Phan Quang Thành <phanquangthanh136@gmail.com>
2024-07-03 23:52:52 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 #### Client-side (Dir: Client App)
 
- change root to 'http://localhost:5000' (domain of server on port 5000).
+change root to 'http://localhost:5000' (domain of server on port 5000).
 
 ```
 
 $ yarn # or npm i     // install packages
 $ npm run dev     // run it locally
+admin_test1
+123456Aa@
+```
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,25 +3,66 @@ import auth from 'src/apis/auth'
 
 import { Formik } from 'formik'
 import { loginSchema } from 'src/untils/formSchema'
+import { TextField, Button, Box, Select, MenuItem, FormControl, InputLabel, FormHelperText } from '@mui/material'
 export default function Login() {
+  const handleSubmit = async (data: { username: string; password: string; company_id: string }) => {
+    await auth.login(data)
+  }
   return (
     <div>
-      <Formik
-        initialValues={{ username: '', password: '', company_id: '' }}
-        validationSchema={loginSchema}
-        onSubmit={(data) => {
-          console.log()
-        }}
-      >
-        {({ values, handleChange, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <input type='text' name='username' value={values.username} onChange={handleChange} />
-            <input type='text' name='password' value={values.password} onChange={handleChange} />
-            <input type='text' name='company_id' value={values.company_id} onChange={handleChange} />
-            <button type='submit'>Submit</button>
-          </form>
-        )}
-      </Formik>
+      <Box display='flex' justifyContent='center' alignItems='center' height='100vh'>
+        <Formik
+          initialValues={{ username: '', password: '', company_id: '' }}
+          validationSchema={loginSchema}
+          validateOnBlur={false}
+          validateOnChange={false}
+          onSubmit={(data) => {
+            console.log(data)
+            handleSubmit(data)
+          }}
+        >
+          {({ values, handleChange, handleSubmit, errors, touched }) => (
+            <form onSubmit={handleSubmit}>
+              <Box mb={2}>
+                <TextField
+                  fullWidth
+                  label='Username'
+                  name='username'
+                  value={values.username}
+                  onChange={handleChange}
+                  error={touched.username && !!errors.username}
+                  helperText={touched.username && errors.username}
+                />
+              </Box>
+              <Box mb={2}>
+                <TextField
+                  fullWidth
+                  label='Password'
+                  name='password'
+                  type='password'
+                  value={values.password}
+                  onChange={handleChange}
+                  error={touched.password && !!errors.password}
+                  helperText={touched.password && errors.password}
+                />
+              </Box>
+              <Box mb={2}>
+                <FormControl fullWidth error={touched.company_id && !!errors.company_id}>
+                  <InputLabel>Company</InputLabel>
+                  <Select label='Company' name='company_id' value={values.company_id} onChange={handleChange}>
+                    <MenuItem value={1}>SBM</MenuItem>
+                    <MenuItem value={2}>DFM</MenuItem>
+                  </Select>
+                  {touched.company_id && errors.company_id && <FormHelperText>{errors.company_id}</FormHelperText>}
+                </FormControl>
+              </Box>
+              <Button type='submit' variant='contained' color='primary'>
+                Submit
+              </Button>
+            </form>
+          )}
+        </Formik>
+      </Box>
     </div>
   )
 }
